32bf31a3b62cc388c082b0c18a80c0df1890c4a5
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-12-18 18:28:10 -0300

    redimensionamento img

--- blog/admin.py
+++ blog/admin.py
@@ -50,3 +50,11 @@ class PostAdmin(admin.ModelAdmin):
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.update_by = request.user
+        else:
+            obj.created_by = request.user
+
+        obj.save()
