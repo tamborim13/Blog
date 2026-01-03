84c9edd2ad0ca0eb5ca40f6f14f3f74a9cb2e74e
tamborim13 <tamborimgiovanni2017@gmail.com>
2026-01-03 14:44:29 -0300

    commit dinamico

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from utils.rands import slugify_new
 from django.contrib.auth.models import User
 from utils.images import resize_image
 from django_summernote.models import AbstractAttachment
+from django.urls import reverse
 
 class PostAttachment(AbstractAttachment):
     def save(self, *args, **kwargs):
@@ -47,7 +48,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=255)
-    slug = models.SlugField( #slug é como se fosse um ID, toda vez que for buscar, vai ser buscado pelo ID
+    slug = models.SlugField( #slug é como se fosse um ID, toda vez que for buscar, vai ser buscado pelo ID(slug)
         unique=True, default=None,
         null=True, blank=True, max_length=255,
     )
@@ -145,6 +146,13 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:post', args=(self.slug,))
+
+    
     
     def save(self, *args, **kwargs):
         if not self.slug:
