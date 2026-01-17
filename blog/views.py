eca1d3c73241f4e8c4c0edc47cce0c82e4a96136
tamborim13 <tamborimgiovanni2017@gmail.com>
2026-01-17 12:26:46 -0300

    commit view

--- blog/views.py
+++ blog/views.py
@@ -118,36 +118,13 @@ class SearchListView(PostListView):
             'page_title' : f'{search_value[:30]} - Search ',
             'search_value': search_value
         })
-        return 
+        return ctx
     
     def get(self, *args, **kwargs):
         if self._search_value == '':
             return redirect('blog:index')
 
         return super().get(*args, **kwargs)
-
-def search(request):
-    search_value = request.GET.get('search', '').strip()
-    posts = (
-        Post.objects.get_published()
-        .filter(
-            Q(title__icontains = search_value) | 
-            Q(excerpt__icontains = search_value) | 
-            Q(content__icontains = search_value) 
-        )[0:PER_PAGE]
-    )
-
-    page_title = f'{search_value[:30]} - Search '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
-            'search_value': search_value,
-            'page_title': page_title,
-        }
-    )
 
 
 def page(request, slug):
